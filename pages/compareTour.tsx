3d862c78b81b67942d04b69d319291c03f07d677
theHTMLSpot <121297730+theHTMLSpot@users.noreply.github.com>
2025-05-12 15:35:43 +0000

    fixes

---
+++ pages/compareTour.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CompareTour({setCurrentStep}: {setCurrentStep: React.SetStateAction<number>}) {
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-between">
+
+        </div>
+    )
+}
